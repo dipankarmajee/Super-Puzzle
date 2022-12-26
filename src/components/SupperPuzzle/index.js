adce4cf336d371ac59e2e37e33667012d3e34831
Dipankar Majee <dipankar.majee@simplyfi.tech>
2022-12-26 12:07:14 +0530

    code updated

--- src/components/SupperPuzzle/index.js
+++ src/components/SupperPuzzle/index.js
@@ -1,36 +1,40 @@
 import { useState, useEffect } from "react";
-import { FetcherWithComponents } from "react-router-dom";
 import "./index.css";
 
-const numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-let suffleNumList = numList.sort(() => Math.random() - 0.5);
+// const numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// let suffleNumList = numList.sort(() => Math.random() - 0.5);
 let totalMoves = 0;
 
 const SupperPuzzle = () => {
-  const [stateNumList, setStateNumList] = useState(suffleNumList);
+  // let numList = [];
+  // let suffleNumList = [];
+  const [stateNumList, setStateNumList] = useState([]);
+  const [matrixSize, setMatrixSize] = useState(0);
+  const [gameStatus, setGameStatus] = useState(false);
 
   const zeroIndex = stateNumList.indexOf(0);
   const adjacentNumers = [
     stateNumList[zeroIndex + 1],
     stateNumList[zeroIndex - 1],
-    stateNumList[zeroIndex - 4],
-    stateNumList[zeroIndex + 4],
+    stateNumList[zeroIndex - matrixSize],
+    stateNumList[zeroIndex + matrixSize],
   ];
 
   const leftColumn = [
     stateNumList[zeroIndex + 1],
-    stateNumList[zeroIndex - 4],
-    stateNumList[zeroIndex + 4],
+    stateNumList[zeroIndex - matrixSize],
+    stateNumList[zeroIndex + matrixSize],
   ];
 
   const rightColumn = [
     stateNumList[zeroIndex - 1],
-    stateNumList[zeroIndex - 4],
-    stateNumList[zeroIndex + 4],
+    stateNumList[zeroIndex - matrixSize],
+    stateNumList[zeroIndex + matrixSize],
   ];
 
   const onClickReplaceNumber = (numId) => {
-    if (zeroIndex % 4 === 0) {
+    // localStorage.setItem("numList", stateNumList);
+    if (zeroIndex % matrixSize === 0) {
       console.log("zeroIndex % 4 === 0 triggered");
       if (leftColumn.includes(numId)) {
         totalMoves += 1;
@@ -47,7 +51,7 @@ const SupperPuzzle = () => {
         });
         setStateNumList(filterNumList);
       }
-    } else if ((zeroIndex + 1) % 4 === 0) {
+    } else if ((zeroIndex + 1) % matrixSize === 0) {
       console.log("(zeroIndex + 1) % 4 === 0 triggered");
       if (rightColumn.includes(numId)) {
         totalMoves += 1;
@@ -85,13 +89,13 @@ const SupperPuzzle = () => {
   };
 
   const numberColor = (eachNum) => {
-    if (zeroIndex % 4 === 0) {
+    if (zeroIndex % matrixSize === 0) {
       if (leftColumn.includes(eachNum)) {
         return "success";
       } else {
         return "danger";
       }
-    } else if ((zeroIndex + 1) % 4 === 0) {
+    } else if ((zeroIndex + 1) % matrixSize === 0) {
       if (rightColumn.includes(eachNum)) {
         return "success";
       } else {
@@ -102,6 +106,19 @@ const SupperPuzzle = () => {
     } else {
       return "danger";
     }
+  };
+
+  const onClickMaxtrix = (matrixVal) => {
+    const numList = [];
+    setMatrixSize(matrixVal);
+    if (matrixVal !== 0) {
+      for (let number = 0; number < Math.pow(matrixVal, 2); number++) {
+        numList.push(number);
+      }
+      setStateNumList(numList.sort(() => Math.random() - 0.5));
+    }
+
+    setGameStatus((prev) => !prev);
   };
 
   return (
@@ -124,30 +141,61 @@ const SupperPuzzle = () => {
         </p>
       </div>
       <div className="puzzle-bg-container">
-        <div className="puzzle-card">
-          {stateNumList.map((eachNum) => {
-            if (eachNum === 0) {
-              return (
-                <button
-                  key={eachNum}
-                  className="number-tiles number-tiles-zero"
-                >
-                  {eachNum}
-                </button>
-              );
-            } else {
-              return (
-                <button
-                  key={eachNum}
-                  className={`number-tiles ${numberColor(eachNum)}`}
-                  onClick={() => onClickReplaceNumber(eachNum)}
-                >
-                  {eachNum}
-                </button>
-              );
-            }
-          })}
-        </div>
+        {!gameStatus && (
+          <div className="matrix-container">
+            <button
+              className="matrix-tiles"
+              type="button"
+              onClick={() => onClickMaxtrix(2)}
+            >
+              2 x 2
+            </button>
+            <button
+              className="matrix-tiles"
+              type="button"
+              onClick={() => onClickMaxtrix(3)}
+            >
+              3 x 3
+            </button>
+            <button
+              className="matrix-tiles"
+              type="button"
+              onClick={() => onClickMaxtrix(4)}
+            >
+              4 x 4
+            </button>
+          </div>
+        )}
+        {gameStatus && (
+          <div
+            className={`puzzle-card ${matrixSize === 2 && "puzzle-card-2-2"} ${
+              matrixSize === 3 && "puzzle-card-3-3"
+            } ${matrixSize === 4 && "puzzle-card-4-4"}`}
+          >
+            {stateNumList.map((eachNum) => {
+              if (eachNum === 0) {
+                return (
+                  <button
+                    key={eachNum}
+                    className="number-tiles number-tiles-zero"
+                  >
+                    {eachNum}
+                  </button>
+                );
+              } else {
+                return (
+                  <button
+                    key={eachNum}
+                    className={`number-tiles ${numberColor(eachNum)}`}
+                    onClick={() => onClickReplaceNumber(eachNum)}
+                  >
+                    {eachNum}
+                  </button>
+                );
+              }
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
